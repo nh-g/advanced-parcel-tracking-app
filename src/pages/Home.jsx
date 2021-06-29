9bc0f9ff8881fb19af7237027abf805d1dcac15d
nh-g <giangngohong@gmail.com>
2021-06-29 15:22:46 +0200

    add home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
+import Card from '../components/Card';
+
 export default function Home({data}) {
+    const userGreeter = `Hi ${data[0].user_name}`
+
+    const CardArray = data.map((item) => {
+        return <Card key = {item.id} parcel={item} />
+    })
     return (
-      <div>
-          Home
-      </div>
+      <section id="home">
+        <header>
+          {" "}
+          <h1> {userGreeter}</h1>
+        </header>
+        <h4 style={{ fontWeight: 300, letterSpacing: 0.5 }}>
+          {" "}
+          We have some parcels for you:{" "}
+        </h4>
+        <div className="order-list">{CardArray}</div>
+      </section>
     );
 }
