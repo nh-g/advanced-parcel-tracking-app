872003f3a6d2965f932ab3d422c8744006ed6642
nh-g <giangngohong@gmail.com>
2021-07-01 09:37:23 +0200

    add Parcel Information component

--- src/pages/ParcelDetail.jsx
+++ src/pages/ParcelDetail.jsx
@@ -1,14 +1,42 @@
+// NPM package
 import React from 'react'
+import { Link, useParams } from 'react-router-dom';
 
+// Project files
+import ParcelInformation from '../components/ParcelInformation';
 export default function ParcelDetail({ data }) {
     console.log("detail page data", [data])
+
+    const { parcel_id} = useParams()
+
+    const selectedParcel = data.find((item) => item.parcel_id === parcel_id)
+
+    const {
+      location_coordinate_latitude: latitude,
+      location_coordinate_longitude: longitude,
+      sender, 
+    } = selectedParcel
+
     return (
-      <div>
-        {data.map((item) => (
-          <div>
-            {item.sender}: {item.status}
-          </div>
-        ))}
-      </div>
+      <section id="order-detail">
+        <header>
+          <h1>
+            Order from {sender}
+          </h1>
+          <p>
+            You can find parcel information below
+          </p>
+        </header>
+
+        <div className="column">
+          <ParcelInformation parcel = {selectedParcel}/>
+        </div>
+
+        <footer className="footer">
+          <Link className="button" to="/">
+            Go back 
+          </Link>
+        </footer>
+      </section>
     );
 }
