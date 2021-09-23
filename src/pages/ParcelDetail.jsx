33870c014889f93fd1a17106e492e875e44ee2d5
nh-g <giangngohong@gmail.com>
2021-09-23 16:05:18 +0200

    null check

--- src/pages/ParcelDetail.jsx
+++ src/pages/ParcelDetail.jsx
@@ -5,30 +5,41 @@ import { Link, useParams } from 'react-router-dom';
 // Project files
 import ParcelInformation from '../components/ParcelInformation';
 import Map from '../components/Map';
-export default function ParcelDetail({ data }) {
-    console.log("detail page data", [data])
 
+export default function ParcelDetail({ data }) {
     const { parcel_id} = useParams()
 
     const selectedParcel = data.find((item) => item.parcel_id === parcel_id)
 
     const {
-      location_coordinate_latitude: latitude,
-      location_coordinate_longitude: longitude,
-      sender,
-    } = selectedParcel;
-
+        location_coordinate_latitude: latitude,
+        location_coordinate_longitude: longitude,
+        sender,
+    } = selectedParcel || {};
+    
     return (
       <section id="parcel-detail">
-        <header>
-          <h1>Order from {sender}</h1>
-          <p>You can find parcel information below</p>
-        </header>
+        {selectedParcel != null && selectedParcel != undefined ? (
+          <>
+            <header>
+              <h1>Order from {sender}</h1>
+              <p>You can find parcel information below</p>
+            </header>
 
-        <div className="columns">
-          <Map coordinates={[latitude, longitude]} parcel={selectedParcel} />
-          <ParcelInformation parcel={selectedParcel} />
-        </div>
+            <div className="columns">
+              <Map
+                coordinates={[latitude, longitude]}
+                parcel={selectedParcel}
+              />
+              <ParcelInformation parcel={selectedParcel} />
+            </div>
+          </>
+        ) : (
+          <div>
+            <h2> Not Found. </h2>
+            <h4>⬅ Please Go back to the list of all parcels.</h4>
+          </div>
+        )}
 
         <footer className="footer">
           <Link className="button" to="/">
