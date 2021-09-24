9b4b9b98b3f2828e268b88fec07edfb71132e255
nh-g <giangngohong@gmail.com>
2021-09-25 00:55:51 +0200

    add button to toggle languages

---
+++ src/components/LanguageSwitcher.jsx
@@ -0,0 +1,34 @@
+//NPM Packages
+import { useState } from "react";
+
+//Project files
+import flag_en from "../assets/images/flag_en.svg";
+import flag_se from "../assets/images/flag_se.svg";
+import i18n from "../i18n";
+
+export default function LanguageSwitcher() {
+  // Local state
+  const [language, setLanguage] = useState("en");
+
+  // Methods
+  function toggleLanguage() {
+    switch (language) {
+      case "en":
+        i18n.changeLanguage("se");
+        setLanguage("se");
+        break;
+      case "se":
+        i18n.changeLanguage("en");
+        setLanguage("en");
+        break;
+    }
+  }
+
+  return (
+    <button className="language-switcher" onClick={toggleLanguage}>
+      <p className="description-small">{language}</p>
+      {language === "en" && <img src={flag_en} alt="British flag" />}
+      {language === "se" && <img src={flag_se} alt="Swedish flag" />}
+    </button>
+  );
+}
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -4,27 +4,33 @@ import {Link} from "react-router-dom";
 //Project files
 import SearchBar from './SearchBar';
 import mapLogo from "../assets/images/map.svg";
+import LanguageSwitcher from "./LanguageSwitcher";
 
 export default function NavigationBar() {
-    return (
-      <nav id="navigation-bar">
-        <Link to="/">
-          <h1>Ptracker</h1>
-        </Link>
+  return (
+    <nav id="navigation-bar">
+      <Link to="/">
+        <h1>Ptracker</h1>
+      </Link>
 
-        <SearchBar />
+      <SearchBar />
 
-        <div className="flexbox-expand-space" />
+      <div className="flexbox-expand-space" />
 
+      <div className="right-items">
         <Link to="/track">
           <img
             className="map-logo"
             src={mapLogo}
+            alt="map"
             title="Track Map"
-            style={{height: "30px"}}
+            style={{ height: "30px" }}
           />
         </Link>
-      </nav>
-    );
+        
+        <LanguageSwitcher/>
+      </div>
+    </nav>
+  );
 }
 
